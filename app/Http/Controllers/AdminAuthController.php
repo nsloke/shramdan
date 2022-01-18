95976cc1b713760367b59fe3a7c4ecafa21dbf86
vaibhavsoshte <vaibhavsoshte@gmail.com>
2022-01-18 22:16:46 +0530

    ked data

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -21,7 +21,6 @@ class AdminAuthController extends Controller
     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     '0', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-
     $len = !$n ? random_int(7, 12) : $n; // Chose length randomly in 7 to 12
 
     $ddd = array_map(function($a) use ($al){
--- app/Http/Controllers/WorksController.php
+++ app/Http/Controllers/WorksController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use GrahamCampbell\ResultType\Result;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Expr\FuncCall;
 
 class WorksController extends Controller
 {
-    //
+    //fetch All Works
+    public function fetchWorks(Request $request)
+    {
+        $informa="[";
+        $informa=$informa."{";
+        $iterwork=0;
+        $resultwork=DB::select("SELECT `workid`, `worktypeid`, `workname`, `worklocation`, `workrequirements`, `workstartdt`, `workenddt` FROM `workstbl` ");
+        if(count($resultwork))
+        {
+            $informa=$informa."\"WorkList\":[";
+        }
+
+        foreach ($resultwork as $work)
+        {
+
+            if($iterwork>0)
+            {
+                $informa=$informa.",";
+            }
+            $informa=$informa."{
+                                \"WorkID\":\"".$work->workid."\",
+                                \"WorkTypeID\":\"".$work->worktypeid."\",
+                                \"WorkName\":\"".$work->workname."\",
+                                \"WorkLocation\":\"".$work->worklocation."\",
+                                \"Workrequirements\":\"".$work->workrequirements."\",
+                                \"Workstartdt\":\"".$work->workstartdt."\",
+                                \"workenddt\":\"".$work->workenddt."\"";
+
+
+                $informa=$informa."}";
+                $iterwork=$iterwork+1;
+
+        }
+        $informa=$informa."]";
+        $informa=$informa."}";
+        $informa=$informa."]";
+        echo $informa;
+
+    }
+
+    // fetch All WorkType
+    public function fetchWorkType(Request $request)
+    {
+    }
 }
