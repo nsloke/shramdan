da0d2b89b003c3358ab1fc70a1a2884336f8c8c5
nsloke <lokesoftwares@gmail.com>
2022-01-19 11:10:42 +0530

    Added Materials and Equipments API

--- app/Http/Controllers/EquipmentsController.php
+++ app/Http/Controllers/EquipmentsController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Redirect;
+
 
 class EquipmentsController extends Controller
 {
     //
     public function fetchEquipmentsByType(Request $request) {
-        $results['data'] = DB::select("select e.equipid, e.workid, from equipmenttbl WHERE equipid=?",[$request->id]);
+        $results['data'] = DB::select("select e.equipid, e.workid, e.eqtypeid, et.equipname, w.workname from equipmenttbl e, equipmenttypetbl et, workstbl w WHERE (w.workid=e.workid AND e.eqtypeid=et.equiptypeid AND et.equiptypeid=?)",[$request->id]);
         return $results;
     }
 
     public function fetchEquipmentsByWork(Request $request) {
-        $results['data'] = DB::select("select subtopicId, subtopicName from equipmenttbl WHERE equipid=?",[$request->id]);
+        $results['data'] = DB::select("select e.equipid, e.workid, e.eqtypeid, et.equipname, w.workname from equipmenttbl e, equipmenttypetbl et, workstbl w WHERE (w.workid=e.workid AND e.eqtypeid=et.equiptypeid AND w.workid=?)",[$request->id]);
         return $results;
     }
 }
--- app/Http/Controllers/MaterialsController.php
+++ app/Http/Controllers/MaterialsController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class MaterialsController extends Controller
 {
     //
+    public function fetchMaterialsByType(Request $request) {
+        $results['data'] = DB::select("select m.materialid, m.workid, m.materialtypeid, mt.materialtypename, w.workname from materialtbl m, materialtypetbl mt, workstbl w WHERE (w.workid=m.workid AND m.materialtypeid=mt.materialtypeid AND m.materialtypeid=?)",[$request->id]);
+        return $results;
+    }
+
+    public function fetchMaterialsByWork(Request $request) {
+        $results['data'] = DB::select("select m.materialid, m.workid, m.materialtypeid, mt.materialtypename, w.workname from materialtbl m, materialtypetbl mt, workstbl w WHERE (w.workid=m.workid AND m.materialtypeid=mt.materialtypeid AND m.workid=?)",[$request->id]);
+        return $results;
+    }
 }
