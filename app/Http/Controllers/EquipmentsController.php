d5dd40dcf0f9a0a8ebe5147af17262b6085aebc7
nsloke <lokesoftwares@gmail.com>
2022-01-21 00:00:50 +0530

    Save Equipment and Admin Functions Added

--- app/Http/Controllers/EquipmentsController.php
+++ app/Http/Controllers/EquipmentsController.php
@@ -22,4 +22,27 @@ class EquipmentsController extends Controller
         $results['data'] = DB::select("select e.equipid, e.workid, e.eqtypeid, et.equipname, w.workname from equipmenttbl e, equipmenttypetbl et, workstbl w WHERE (w.workid=e.workid AND e.eqtypeid=et.equiptypeid AND w.workid=?)",[$request->id]);
         return $results;
     }
+
+    public function saveEquipments(Request $request)
+    {
+        $equipid=$request->post('equipid');
+        $eqtypeid=$request->post('eqtypeid');
+        $workid=$request->post('workid');
+        $stock=$request->post('stock');
+        $remarks=$request->post('remarks');
+
+
+        $saveEquipments=DB::insert('INSERT INTO equipmenttbl(equipid,eqtypeid,workid,stock,remarks) VALUES (?,?,?,?,?)',[$equipid,$eqtypeid,$workid,$stock,$remarks]);
+
+        if($saveEquipments)
+        {
+            echo "Data Inserted Successfully";
+        }
+        else
+         {
+         echo "Insertion Error";
+         }
+
+    }
+
 }
